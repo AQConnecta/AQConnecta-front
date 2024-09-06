ff06499977d22daf6b2a57f65269ea1a150ec9b7
Riume <riumecarlos@hotmail.com>
2024-09-05 23:53:34 -0300

    adicionando filtro de iniciante

--- src/layout/components/Left.tsx
+++ src/layout/components/Left.tsx
@@ -1,12 +1,28 @@
-import { Box } from '@mui/material'
-import HotCompentencias from '../../pages/Home/components/HotCompetencias'
+import { Box, Checkbox, FormControlLabel } from '@mui/material';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import HotCompentencias from '../../pages/Home/components/HotCompetencias';
 
 function Left() {
+  const [iniciante, setIniciante] = useState(false);
+  const navigate = useNavigate();
+
+  const handleCheckboxChange = (event: any) => {
+    const isChecked = event.target.checked;
+    setIniciante(isChecked);
+    navigate(`/buscar?tipo=vagas&iniciante=${isChecked}`);
+  };
+
   return (
     <Box>
       <HotCompentencias />
+      
+      <FormControlLabel
+        control={<Checkbox checked={iniciante} onChange={handleCheckboxChange} />}
+        label="Mostrar apenas vagas para iniciantes"
+      />
     </Box>
-  )
+  );
 }
 
-export default Left
+export default Left;
